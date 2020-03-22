4b5f371c32cc4ac115656469b096e50f4c4d3747
FRZ <faraz.ar.sheikh@gmail.com>
2020-03-22 19:41:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/splashscreen/Splash.java
@@ -0,0 +1,37 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread thread = new Thread()
+        {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+        thread.start();
+
+    }
+}

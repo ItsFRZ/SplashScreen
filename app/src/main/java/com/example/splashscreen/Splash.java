28756d6f4c8ef397d6ec52470752eebc894b2fd8
FRZ <61186175+ItsFRZ@users.noreply.github.com>
2020-03-30 20:39:28 +0530

    Update Splash.java

--- app/src/main/java/com/example/splashscreen/Splash.java
+++ app/src/main/java/com/example/splashscreen/Splash.java
@@ -19,7 +19,7 @@ public class Splash extends AppCompatActivity {
             public void run() {
                 super.run();
                 try {
-                    sleep(4000);
+                    sleep(4000); // By using sleep value you can control the timing of splash screen.
                     Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
